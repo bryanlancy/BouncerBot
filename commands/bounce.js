2887f63b5891a07b21fa1de354db7677e2aae6d1
Bryan <bryanburns93@gmail.com>
2020-12-11 22:31:50 -0500

    moved bounce command to separate file

--- commands/bounce.js
+++ commands/bounce.js
@@ -1,5 +1,4 @@
-const { MessageEmbed } = require("discord.js");
-
+const { MessageEmbed } = require("discord.js")
 
 function bounce(bot, mes, args) {
     let daysToKick = 3; //!Number of days to be in server to be eligible for bouncing
@@ -7,20 +6,18 @@ function bounce(bot, mes, args) {
         .setTitle('**Auto-Bounce Summary**')
         .setColor('#0055ff')
 
-
     bot.client.guilds.fetch(mes.guild.id)
         .then(guild => guild.members.fetch())
         .then(members => {
             let list = []
             let now = new Date()
-            console.log(now)
-            for (member of members) {
-                if (member[1]._roles.length < 1) {
+            for (mem of members) {
+                if (mem[1]._roles.length < 1) {
                     //difference in days, between today and member join date
-                    let d = Math.floor(((now - new Date(member[1].joinedTimestamp)) / 86400000)) //?24(hours) * 60(minutes) * 60(seconds) * 1000(milliseconds)
-                    if (d > daysToKick) {
-                        let name = `<@${member[1].user.id}>`
-                        list.push(`${name} - *Time in server: ${d} days*`)
+                    let daysInServer = Math.floor(((now - new Date(mem[1].joinedTimestamp)) / 86400000)) //?24(hours) * 60(minutes) * 60(seconds) * 1000(milliseconds)
+                    if (daysInServer > daysToKick) {
+                        let name = `<@${mem[1].user.id}>`
+                        list.push(`${name} - *Time in server: ${daysInServer} days*`)
                     }
                 }
             }
