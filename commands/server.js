f742a6aace6ddd8144b8af5afa7ac72d732e3034
Bryan Burns <bryanburns93@gmail.com>
2020-12-12 18:11:57 -0500

    rename embed variable

--- commands/server.js
+++ commands/server.js
@@ -1,14 +1,15 @@
 const { MessageEmbed } = require("discord.js")
 
 function server(mes, args, bot) {
-    let embedServer = new MessageEmbed()
+    let embed = new MessageEmbed()
         .setTitle('**Server Summary**')
         .setColor('#0055ff')
     bot.client.guilds.fetch(mes.guild.id)
-        //!WILL FAIL TO RETURN PROPER RESULTS IF MEMBER COUNT EXCEEDS ?
+        //!WILL FAIL TO RETURN PROPER RESULTS IF MEMBER COUNT EXCEEDS ?*NEED TO FIND AMOUNT*
         //TODO: Create more robust process to handle higher counts
         .then(guild => {
-            //TODO More robust member search may require multiple fetch's so 'i' is declared here for now
+            //TODO: More robust member search may require multiple fetch's so 'i' is declared here for now
+            //TODO: If more than ? amount, push to member array and fetch again, when less than ? amount, push then return
             let i = 0
             guild.members.fetch()
                 .then(
@@ -22,11 +23,11 @@ function server(mes, args, bot) {
                                 list.push(`${name} - *Join Date: ${d.toDateString()}*`)
                             }
                         }
-                        embedServer.addFields(
+                        embed.addFields(
                             { name: 'Total Members', value: i },
                             { name: 'Users without a role', value: list.length ? list : 'None!' }
                         )
-                        mes.channel.send({ embed: embedServer, split: true })
+                        mes.channel.send({ embed: embed, split: true })
                     })
         }
         )
