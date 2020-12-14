8c4c831f8f37ea8f970cd0101f83d062bc065e85
Bryan Burns <bryanburns93@gmail.com>
2020-12-14 13:06:09 -0500

    add addDefaultText functino

--- config/Bot.js
+++ config/Bot.js
@@ -1,38 +1,45 @@
 const { Client, Intents } = require('discord.js')
-const intents = new Intents([Intents.NON_PRIVILEGED, "GUILD_MEMBERS"])
+const intents = new Intents([Intents.NON_PRIVILEGED, 'GUILD_MEMBERS'])
 
 const config = require('./config.json')
 const commands = require('./commands') //? bot command folder, each command imported in commands/index.js
 
 class Bot {
-    constructor(addCommands) {
-        this.config = config
-        this.client = new Client({ ws: { intents } })
-        //! Option for lightweight client if needed
-        if (addCommands) {
-            for (const c in commands) {
-                if (!this.hasOwnProperty[c]) this[c] = commands[c]
-            }
-            this.parseCommand = function(message){
-                const args = message.content.slice(this.config.prefix.length).trim().split(/ +/g)
-                const command = args.shift().toLowerCase()
-                return [command, args]
-            }
-            this.botCommand = function(message){
-                const [command, args] = this.parseCommand(message)
-                if(this.hasOwnProperty(command)){
-                    try {
-                        this[command](message, args, this)
-                    } catch (error) {
-                        console.log(error)
-                    }
-                }
-            }
-        }
-    }
+	constructor(addCommands) {
+		this.config = config
+		this.client = new Client({ ws: { intents } }).addDefaultText()
+
+		//! Option for lightweight client if needed
+		if (addCommands) {
+			for (const c in commands) {
+				if (!this.hasOwnProperty[c]) this[c] = commands[c]
+			}
+			this.parseCommand = function (message) {
+				const args = message.content
+					.slice(this.config.prefix.length)
+					.trim()
+					.split(/ +/g)
+				const command = args.shift().toLowerCase()
+				return [command, args]
+			}
+			this.botCommand = function (message) {
+				const [command, args] = this.parseCommand(message)
+				if (this.hasOwnProperty(command)) {
+					try {
+						this[command](message, args, this)
+					} catch (e) {
+						return new Error(
+							`Failed to execute the ${command} command\n${e}`
+						)
+					}
+				}
+			}
+		}
+	}
+	addDefaultText() {}
 }
 try {
-    module.exports = Bot
+	module.exports = Bot
 } catch (error) {
-    module.exports = null
+	module.exports = null
 }
