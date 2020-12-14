d9d5cfeb6b057b2f6aab986881cd2215fbb7fc82
Bryan Burns <bryanburns93@gmail.com>
2020-12-14 13:09:46 -0500

    reformat index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const bB = new (require('./config/Bot.js'))(true) //? Bot instantiation
 bB.client.once('ready', () => {
-    console.log(`${bB.config.botName} - Online`) //? For local testing, to ensure bot successfully comes online
+	console.log(`${bB.config.botName} - Online`) //? For local testing, to ensure bot successfully comes online
 })
-bB.client.on('message', message => {
-    if (message.content.startsWith(`${bB.config.prefix}`)) {
-        bB.botCommand(message)
-    }
+bB.client.on('message', (message) => {
+	if (message.content.startsWith(`${bB.config.prefix}`)) {
+		bB.botCommand(message)
+	}
 })
 bB.client.login(bB.config.token)
